63b499f388161f10a16b110181d52368b9467dd1
PammeC <pamelajnth@gmail.com>
2026-01-28 14:54:38 -0500

    registerActivity

--- app/src/main/java/com/example/electromarket/RegisterActivity.kt
+++ app/src/main/java/com/example/electromarket/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.electromarket
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
